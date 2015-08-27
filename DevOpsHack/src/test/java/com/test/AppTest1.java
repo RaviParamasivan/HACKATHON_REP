791d1b710eab72b1d3d3cd0284d704316444af21
Administrator <Administrator@scsbwin-672789.ITTPA.EBIZ.VERIZON.COM>
2015-08-27 03:40:16 -0700

    devops

---
+++ DevOpsHack/src/test/java/com/test/AppTest1.java
@@ -0,0 +1,16 @@
+package com.test;
+import static org.junit.Assert.*; 
+import org.junit.Test;
+public class AppTest1 {
+
+	@Test
+	public void getText(){
+		// TODO Auto-generated method stub
+		AppTest test = new AppTest();
+		String x=test.getTest();
+		assertNotNull(x);
+		
+
+	}
+
+}
